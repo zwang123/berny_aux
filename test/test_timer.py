fccbdcea399dfb5d871e14adbe509110b675d03f
zwang123 <zhiwang@uchicago.edu>
2018-09-25 22:48:25 -0500

    completed/debugged timer and test_timer

--- test/test_timer.py
+++ test/test_timer.py
@@ -1,56 +1,68 @@
-from multiprocessing import Pool
-from os import cpu_count
-from berny_aux.timer import Timer
-from math import sqrt
+from berny_aux.timer import Timer, Time
+from time import process_time, sleep
+# importing the required module 
+import timeit 
 
-#def f(x):
-#    y=0.0
-#    y+=x*x
-#    return y
-#def f(x):
-#    y = 0.0
-#    for i in range(10000000):
-#        y+=x*x
-#    return y
 
 if __name__ == '__main__':
-    print(cpu_count())
-    t = Timer()
-    t.start()
-    with Pool() as p:
-        #result = (p.map(f, list(range(cpu_count()))))
-        result = (p.map(sqrt, list(range(cpu_count()))))
-    #with Pool() as p:
-    #    #f = lambda x: x*x
-    #    for i in range(1000):
-    #        #result = (p.map((lambda x: (x*x)), list(range(cpu_count()))))
-    #        result = (p.map(f, list(range(cpu_count()))))
-    t.stop()
-    print(t.elapsed_time.wall_time)
-    print(t.elapsed_time.process_time)
-    
-    # importing the required module 
-    import timeit 
       
     # code snippet to be executed only once 
     mysetup = """
-from multiprocessing import Pool
-from os import cpu_count
-from math import sqrt
-"""
-      
+    from multiprocessing import Pool
+    from os import cpu_count
+    from math import sqrt
+    """
+          
     # code snippet whose execution time is to be measured 
     mycode = """
-with Pool() as p:
-    result = (p.map(sqrt, list(range(cpu_count()))))
-"""
-      
+    with Pool() as p:
+        result = (p.map(sqrt, list(range(cpu_count()))))
+    """
+    
+    eval(repr(Time()))
+
+    number = 10
+
+    exec(mysetup, globals(), locals())
+    print("cpu count:", cpu_count())
+    tmp = Timer().start()
+    t = Timer()
+    t1 = Timer()
+    for _ in range(2):
+        t.reset()
+        t1.reset()
+        t.start()
+        t1.start()
+        for i in range(number):
+            exec(mycode, globals(), locals())
+            t.split()
+            t1.split()
+        t.stop()
+        print(t)
+        sleep(0.5)
+        print(t1)
+        print("Timer wall time", t.elapsed_time.wall_time/number)
+        print("Timer process time", t.elapsed_time.process_time/number)
+        #print("Timer split time", t.split_time)
+        #print(repr(t.split_time))
+    
     # timeit statement 
-    print(timeit.timeit(setup = mysetup, 
+    print("timeit wall time",
+          timeit.timeit(setup = mysetup, 
                         stmt = mycode, 
-                        number = 10) * 0.1)
+                        number = number) / number)
+    
+    # timeit statement 
+    print("timeit process time",
+          timeit.timeit(setup = mysetup, 
+                        stmt = mycode, 
+                        timer = process_time,
+                        number = number) / number)
 
     # timeit statement 
-    print(timeit.timeit(setup = "from berny_aux.timer import Timer; t = Timer()", 
-                        stmt = "t.start(); t.split(); t.stop(); t.elapsed_time; t.split_time", 
-                        number = 10000 ) * 0.0001)
+    print("Timer method time",
+          timeit.timeit(setup = "from berny_aux.timer import Timer;"+\
+                                " t = Timer()", 
+                        stmt = "t.start(); t.split(); t.stop();"+\
+                               " t.elapsed_time; t.split_time", 
+                        number = 20000 ) * 0.00005)
--- timer.py
+++ timer.py
@@ -1,19 +1,36 @@
 import time
 
 class Timer(object):
-    """ A Timer class that supports start/stop/split/reset """
+    """ A Timer class that supports start/stop/split/reset 
+        error < 2 ms
+        func call overhead < 0.05 ms unless len(self.split_time) > 10000
+    """
 
-    def __init__(self):
+    def __init__(self, msg=''):
         """ initialize and reset a timer """
+        self.msg = msg
         self.reset()
+
+    def __str__(self):
+        elapsed = self.elapsed_time
+        split = self.split_time
+        for_msg = ' for ' + self.msg if self.msg else ''
+        return \
+        '    total    : wall time{} {:10.3f} sec, process time {:10.3f} sec\n' \
+                .format(for_msg, 
+                        elapsed.wall_time, elapsed.process_time) + \
+        '\n'.join(
+        '    split {:3d}: wall time{} {:10.3f} sec, process time {:10.3f} sec' \
+                .format(i, for_msg, t.wall_time, t.process_time) \
+                for i, t in enumerate(split)
+        )
 
     def reset(self):
         """ reset to zero, clear split, and stop """
         # self.__now only valid if self.stopped
         self.__zero = self.__get_now()
-        self.__split = [] #???
+        self.__split = [] #or numpy.ndarray?
         self.__split_total = Time()
-        #self.__last = self.__zero
         self.__now = self.__zero
         self.__stopped = True
         return self
@@ -23,7 +40,6 @@ class Timer(object):
         if self.stopped:
             stopped_time = self.__get_now() - self.__now
             self.__zero += stopped_time
-            #self.__last += stopped_time
             self.__stopped = False
         return self
 
@@ -38,10 +54,7 @@ class Timer(object):
         """ split the timer if started, does not stop """
         if not self.stopped:
             self.__split.append(self.get_split())
-            self.__split = self.split_time
             self.__split_total += self.__split[-1]
-            #pass
-            #self.__last = self.__get_now()
         return self
 
     def get_split(self):
@@ -58,7 +71,6 @@ class Timer(object):
 
     @property
     def split_time(self):
-        #return self.__split + [self.elapsed_time - np.sum(self.__split)]
         return self.__split + [self.get_split()]
 
     @classmethod
@@ -81,12 +93,25 @@ class Time(object):
     def __sub__(self, other):
         return Time(self.wall_time - other.wall_time, 
                     self.process_time - other.process_time)
+        
+    def __repr__(self):
+        return '{}(wall_time={}, process_time={})'.format(
+                self.__class__.__name__, self.wall_time, self.process_time)
+        
+    def __str__(self):
+        return self.__repr__()
 
 #from numpy import ndarray, array
 #class Time(ndarray):
-#    def __init__(self, wall_time=0.0, process_time=0.0):
-#        #super().__init__((wall_time, process_time), dtype=float)
-#        self = array((wall_time, process_time), dtype=float)
+#    def __new__(cls, wall_time=0.0, process_time=0.0):
+#        return super(Time, cls).__new__(cls, 
+#                (2,), dtype=float, 
+#                buffer=array((wall_time, process_time), dtype=float))
+#
+#    #def __init__(self, wall_time=0.0, process_time=0.0):
+#    #    print(array((wall_time, process_time), dtype=float))
+#    #    super().__init__((2,), dtype=float, buffer=(wall_time, process_time))
+#    #    #self = array((wall_time, process_time), dtype=float)
 #
 #    @property
 #    def wall_time(self):
